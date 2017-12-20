71dd1e1b3f1e6e1b137facf94b846e23da65f7ce
pythonic <936968629@qq.com>
2017-12-20 13:50:28 +0800

    wen

--- common/service/StaticService.php
+++ common/service/StaticService.php
@@ -2,7 +2,24 @@
 namespace app\common\service;
 
 class StaticService{
+    public static function includeAppJsStatic($path,$depend){
+        self::includeAppStatic("js",$path,$depend);
+    }
 
+    public static function includeAppCssStatic($path,$depend){
+        self::includeAppStatic("css",$path,$depend);
+    }
 
+    public static function includeAppStatic($type,$path,$depend){
+        //加入版本号
+        $release_version = defined("RELEASE_VERSION")?RELEASE_VERSION:time();
+        $path = $path."?ver=".$release_version;
 
+        if($type=="css"){
+            \Yii::$app->getView()->registerCssFile($path,['depends'=>$depend]);
+        }
+        else if($type=="js"){
+            \Yii::$app->getView()->registerJsFile($path,['depends'=>$depend]);
+        }
+    }
 }
