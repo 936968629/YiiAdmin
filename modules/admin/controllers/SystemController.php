9d6873130f08cf50dfb264b1e979c57a05875922
pythonic <936968629@qq.com>
2017-12-26 14:57:12 +0800

    wen

---
+++ modules/admin/controllers/SystemController.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/12/26
+ * Time: 14:19
+ */
+
+namespace app\modules\admin\controllers;
+
+
+use app\modules\admin\controllers\common\BaseController;
+
+class SystemController extends BaseController
+{
+    /*菜单管理*/
+    public function actionMenu(){
+
+
+
+
+        $this->render('menu',[]);
+    }
+
+
+}
