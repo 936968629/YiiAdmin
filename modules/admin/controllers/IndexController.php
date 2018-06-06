ff1d0e2b19a4203cc49de185053e4f81cc2756bf
251328109 <251328109@qq.com>
2018-06-06 19:16:05 +0800

    bug修改

--- modules/admin/controllers/IndexController.php
+++ modules/admin/controllers/IndexController.php
@@ -58,18 +58,19 @@ class IndexController extends BaseController {
                 for($i = 0; $i <= $count_day; $i++){
                     $day = $timeOneStart + $i*86400; //第n天日期
                     $day_after = $timeOneStart + ($i+1)*86400; //第n+1天日期
-                    $dates = date('Y-m-d',$timeOneStart + $i*86400); //第n天日期
-                    $datee = date('Y-m-d',$timeOneStart + ($i+1)*86400); //第n+1天日期
+                    $dates = date('Y-m-d H:i:s',$timeOneStart + $i*86400); //第n天日期
+                    $datee = date('Y-m-d H:i:s',$timeOneStart + ($i+1)*86400); //第n+1天日期
 
                     $recharge_date[] = date('m月d日', $day);
-                    $sql = "select COUNT(*) count from bs_order where create_time >= ".$dates." and create_time<".$datee." and status not in (0)";
+                    $sql = "select COUNT(*) count from bs_order where create_time >= '".$dates."' and create_time<'".$datee."' and status not in (0)";
 
                     $connection  = \Yii::$app->db;
                     $command = $connection->createCommand($sql);
                     $res     = $command->queryOne();
+
                     $count = intval( $res['count'] );
                     $total += $count;
-                    $recharge_count[] = 0;
+                    $recharge_count[] = $count;
                 }
                 $newlist1 = $recharge_date;
                 $newlist2 = $recharge_count;
